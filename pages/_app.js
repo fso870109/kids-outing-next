40da827ec2cf7d640081cb038922cdb9a2939ee6
Frank Song <65116656+fso870109@users.noreply.github.com>
2026-06-24 10:14:56 +0800

    加入 Google Analytics G-MCQ7GWXM2W

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,38 @@
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import Script from "next/script";
+
+const GA_ID = "G-MCQ7GWXM2W";
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      if (typeof window.gtag !== "undefined") {
+        window.gtag("config", GA_ID, { page_path: url });
+      }
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => router.events.off("routeChangeComplete", handleRouteChange);
+  }, [router.events]);
+
+  return (
+    <>
+      {/* Google Analytics */}
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
+        strategy="afterInteractive"
+      />
+      <Script id="ga-init" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${GA_ID}', { page_path: window.location.pathname });
+        `}
+      </Script>
+      <Component {...pageProps} />
+    </>
+  );
 }
